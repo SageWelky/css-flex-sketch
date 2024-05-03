b71aaa4e213e83e55bc5116dcadb0ae03f1aa0cd
Sage Welky <ehrenwelky@gmail.com>
2024-05-03 15:26:17 -0500

    Removed logging that was no longer needed, and hooked up the logic for drawing by replacing placeholder "hover" and externalized function. I did not want to add a real hover event listener while tinkering with the grid layout code, but did want to write down my basic idea for the code functionality.

--- scripts.js
+++ scripts.js
@@ -1,15 +1,14 @@
 let containerGrids = document.querySelector( "#container-grids" );
 let buttonAddGridAmount = document.querySelector( "#button-grid-number" );
 let buttonResetDrawing = document.querySelector( "#button-reset-drawing");
+let currentHoveredButton = [];
 
 
-
-containerGrids.addEventListener( "hover", ( event ) => {
+containerGrids.addEventListener( "mouseover", ( event ) => {
 
   let hoveredGrid = event.target;
   event.preventDefault;
-
-  hoverReaction( hoveredGrid.id );
+  hoveredGrid.classList.add("active-grid");
 
 });
 
@@ -29,19 +28,8 @@ buttonAddGridAmount.addEventListener( "click", () => {
 
   gridAmountValid = false;
   gridSetter( parseInt( squaresPerSide ) );
-  console.log("grid sent");
 
 });
-
-
-function hoverReaction( gridID ) {
-
-  console.log(gridID);
-  let currentHoveredButton = document.querySelector(`#${gridID}`);
-  currentHoveredButton.classList.add("active-grid");
-
-}
-
 
 
 function gridSetter( numberOfSquaresPerSide ) {
@@ -53,24 +41,17 @@ function gridSetter( numberOfSquaresPerSide ) {
   let divs = [];
   let div;
 
-  console.log( "sides sent: ", numberOfSquaresPerSide);
-
   while ( j < ( numberOfSquaresPerSide + 1 ) ) {
-    console.log("row: ", j);
-    console.log( "sides sent: ", numberOfSquaresPerSide);
 
     for ( let i = 1; i < ( numberOfSquaresPerSide + 1 ); i++ ) {
 
       divs[i - 1] = div;
-      console.log("box id: ", div);
       div = document.createElement( "div" );
       squareID = j * i;
       div.setAttribute( "id", `grid-${squareID.toString()}` );
       div.style.width = `${100 / numberOfSquaresPerSide}%`;
       div.classList.add("grid-square");
       containerGrids.appendChild( div );
-      console.log("column: ", i);
-      console.log( "sides sent: ", numberOfSquaresPerSide);
     }
 
     j++
